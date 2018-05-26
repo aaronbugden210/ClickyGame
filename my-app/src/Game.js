03ab93f47f2464e70e67bd37ac67205684fdb140
Aaron Bugden <aaronbugden@gmail.com>
2018-05-25 22:02:28 -0400

    Final Commit

--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -5,6 +5,8 @@ const buttonArr = ["_images/bobafett.jpeg", "_images/chewie.jpeg", "_images/fin.
                    "_images/leia.jpeg", "_images/luke.jpeg", "_images/monmothma.jpeg",
                    "_images/rey.jpeg", "_images/vader.jpeg", "_images/yoda.jpeg"];
 
+var score = 0;
+
 class Game extends Component
 {
     render()
@@ -13,22 +15,22 @@ class Game extends Component
             <div>
                 <table>
                     <tr>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
                     </tr>
                     <tr>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
                     </tr>
                     <tr>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
-                        <td><button class = "tableButton"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
+                        <td><button class = "tableButton" onClick = "buttonClick()"></button></td>
                     </tr>
                 </table>
             </div>    
@@ -36,9 +38,9 @@ class Game extends Component
     }
 }
 
-//I could not get this function to work properly
+//I could not get these functions to work properly
 //the intended goal was to have a function that would populate a table with a random image from the array created up top
-//
+//the button click function was meant to create
 function fillTable()
 {
     shuffleArray(buttonArr);
@@ -54,3 +56,27 @@ function shuffleArray(array)
         array[j] = tempArr;
     }
 }
+
+function buttonClick()
+{
+    //if (button is clicked && was not already clicked)
+    //{
+    //  score++;
+    //  fillTable();
+    //}
+    //
+    //else if( button is clicked && was already clicked )
+    //{
+    //  gameOver();
+    //}
+    //
+}
+
+function gameOver()
+{
+    //Display You Lose;
+    //score = 0;
+    //fillTable();
+}
+
+export default Game;
